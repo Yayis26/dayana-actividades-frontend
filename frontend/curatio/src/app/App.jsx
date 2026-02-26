a48424563cfd4df2229850ff7f1c513f772348d3
Nicolasecheverry22 <Nicolasecheverry52@gmail.com>
2026-02-25 19:11:05 -0500

    feat:Componentes sin react router y sin navbar

---
+++ frontend/curatio/src/app/App.jsx
@@ -0,0 +1,26 @@
+//Se comenta para que no entre en conflicto con el CreateUserPage, que es el que se va a mostrar en la pagina de crear usuario
+//import UserForm from "../features/users/components/UserForm";
+import UserForm from "../features/users/components/UserForm";
+import CreateUserPage from "../features/users/pages/CreateUserPage";
+import DeleteCountter from "../shared/components/DeleteCountter.jsx";
+import DeleteUseEffect from "../shared/components/DeleteUseEffect.jsx";
+import Select from "../shared/components/Select.jsx";
+
+export default function App() {
+
+    return(
+        <div className="grid grid-cols-1 gap-6  justify-items-center">
+            <h1 className="text-tittles font-bold text-center text-label">
+                Crear Usuario             
+            </h1>
+
+            <div className="">
+                <CreateSuppliers />
+                {/* <CreateUserPage/> */}
+                {/* <DeleteCounter /> */}
+                {/* <DeleteUseEffect /> */}
+                {/* <Select/> */}
+            </div>
+        </div>
+    );
+};
